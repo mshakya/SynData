796955a568e7ae77f3c331aea98405c23cc7c66f
Migun Shakya <microbeatic@gmail.com>
2017-10-30 14:24:59 -0600

    test files

--- tests/test_call_art_illumina.py
+++ tests/test_call_art_illumina.py
@@ -4,14 +4,30 @@
 
 from __future__ import print_function
 import pytest
-from syndata import call_art_illumina
+from syndata import art_illumina
 
 
 def test_call_art_illumina():
-    x = call_art_illumina(IN="test.fna", out="out_data")
-    print(x)
-    assert len(x) == 10
+    """
+    Test call_art_illumina outputs right set of option.
+
+    this only test if the function parses it correctly
+    """
+    x = art_illumina.call_art_illumina(in_fasta="test.fna", art_para_dic={"errfree": True,
+                                                             "paired": True,
+                                                             "fcov": 11})
+    assert len(x) == 6
 
 def test_call_art_illumina2():
-    x = call_art_illumina(IN="data/E_coli.fasta", out="out_data")
-    assert x == ['--in', 'data/E_coli.fasta', '--seqSys', 'MSv1', '--len', 250, '--fcov', 10, '--out', 'out_data']
+    """
+    Test call_art_illumina outputs right set of option.
+
+    this only test if the function parses it correctly
+    """
+
+    x = art_illumina.call_art_illumina(in_fasta="data/E_coli.fasta", art_para_dic={"seqSys": "MSv1",
+                                                                 "paired": True,
+                                                                 "fcov": 11,
+                                                                 "len": 250})
+    assert 'MSv1' in ['--in', 'data/E_coli.fasta', '--seqSys', 'MSv1', '--paired',
+                      '--fcov', '11', '--len', 250]
