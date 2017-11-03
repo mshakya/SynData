7bac4c6cc874adf16dcb5270ca4cfcba35cb7543
Migun Shakya <microbeatic@gmail.com>
2017-11-02 21:47:26 -0600

    miniconda 3

--- INSTALL.sh
+++ INSTALL.sh
@@ -70,7 +70,7 @@ if [[ "$OSTYPE" == "darwin"* ]]
 then
 {
 
-  curl -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda2-4.2.12-MacOSX-x86_64.sh
+  curl -o miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-4.2.12-MacOSX-x86_64.sh
   chmod +x miniconda.sh
   ./miniconda.sh -b -p $ROOTDIR/thirdParty/miniconda -f
   ln -sf $ROOTDIR/thirdParty/miniconda/bin/conda $ROOTDIR/bin/conda
@@ -80,7 +80,7 @@ then
 else
 {  
 
-  wget https://repo.continuum.io/miniconda/Miniconda2-4.2.12-Linux-x86_64.sh -O miniconda.sh
+  wget https://repo.continuum.io/miniconda/Miniconda3-4.2.12-Linux-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
   ./miniconda.sh -b -p $ROOTDIR/thirdParty/miniconda -f
   ln -sf $ROOTDIR/thirdParty/miniconda/bin/conda $ROOTDIR/bin/conda
